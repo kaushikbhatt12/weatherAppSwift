7df406c1c0920c7a3fb1b813d2ca1719c2319ffd
Kaushik Bhatt <kaushikbhat791@gmail.com>
2024-09-08 02:30:14 +0530

    added sunrise card

---
+++ SunriseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SunriseCollectionViewCell.swift
+//  Project-Demo
+//
+//  Created by kaushik.bha on 08/09/24.
+//
+
+import UIKit
+
+class SunriseCollectionViewCell: UICollectionViewCell {
+    
+}
