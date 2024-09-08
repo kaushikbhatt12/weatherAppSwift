b8049a3fc38fb1e4bb1ed0fc8b1748b03d72ffed
Kaushik Bhatt <kaushikbhat791@gmail.com>
2024-09-08 17:53:53 +0530

    Added all cards

---
+++ Project-Demo/SunsetCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SunsetCollectionViewCell.swift
+//  Project-Demo
+//
+//  Created by kaushik.bha on 08/09/24.
+//
+
+import UIKit
+
+class SunsetCollectionViewCell: UICollectionViewCell {
+    
+}
