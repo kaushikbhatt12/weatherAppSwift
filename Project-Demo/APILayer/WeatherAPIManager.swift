f9de37382de77219fd7b5d9281a8c32efd2877be
Kaushik Bhatt <kaushikbhat791@gmail.com>
2024-09-11 15:39:49 +0530

    Code Refactor

--- Project-Demo/APILayer/WeatherAPIManager.swift
+++ Project-Demo/APILayer/WeatherAPIManager.swift
@@ -12,17 +12,17 @@ class APIManager {
     static let API_KEY = ApiConstants.API_KEY
     
     static func getCitySearchAPIEndpoint(cityName: String) -> String {
-        let apiUrlStr = String(format: ApiConstants.getCoordinatesApiEndPoint,cityName,API_KEY)
+        let apiUrlStr = String(format: ApiConstants.getCoordinatesApiEndPoint,ApiConstants.BASE_URL ,cityName,API_KEY)
         return apiUrlStr
     }
 
     static func getWeatherAPIEndpoint(latitude: Double, longitude: Double) -> String {
-        let apiUrlStr = String(format: ApiConstants.getWeatherForCoordinatesApiEndPoint, String(latitude), String(longitude), API_KEY )
+        let apiUrlStr = String(format: ApiConstants.getWeatherForCoordinatesApiEndPoint,ApiConstants.BASE_URL, String(latitude), String(longitude), API_KEY )
         return apiUrlStr
     }
     
     static func getIconEndPoint(imageParameter : String) -> String {
-        let apiUrlStr  = String(format: ApiConstants.fetchImageEndPoint, imageParameter)
+        let apiUrlStr  = String(format: ApiConstants.fetchImageEndPoint,ApiConstants.BASE_URL, imageParameter)
         return apiUrlStr
     }
 }
--- Project-Demo/Constants/AppConstants.swift
+++ Project-Demo/Constants/AppConstants.swift
@@ -29,9 +29,9 @@ struct ApiConstants {
     static let GET = "GET"
     static let API_KEY = ProcessInfo.processInfo.environment["apiKey"]!
     static let BASE_URL = "https://api.openweathermap.org"
-    static let getCoordinatesApiEndPoint = "\(BASE_URL)/geo/1.0/direct?q=%@&limit=1&appid=%@"
-    static let getWeatherForCoordinatesApiEndPoint = "\(BASE_URL)/data/2.5/weather?lat=%@&lon=%@&appid=%@"
-    static let fetchImageEndPoint = "\(BASE_URL)/img/w/%@.png"
+    static let getCoordinatesApiEndPoint = "%@/geo/1.0/direct?q=%@&limit=1&appid=%@"
+    static let getWeatherForCoordinatesApiEndPoint = "%@/data/2.5/weather?lat=%@&lon=%@&appid=%@"
+    static let fetchImageEndPoint = "%@/img/w/%@.png"
 }
 
 @objc class Messages : NSObject {
