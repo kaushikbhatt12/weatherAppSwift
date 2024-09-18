45a7c7a7e634e11b41e790010e41c937a2998053
Kaushik Bhatt <kaushikbhat791@gmail.com>
2024-09-18 15:18:01 +0530

    Refactored temperature cell and humidity cell

--- HumidityCollectionViewCell.swift
+++ HumidityCollectionViewCell.swift
@@ -2,15 +2,47 @@ import UIKit
 
 class HumidityCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var backgroundImageView: UIImageView!
-    @IBOutlet weak var humidityLabel: UILabel!
+    let backgroundImageView = UIImageView()
+    let humidityLabel = UILabel()
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpLayout()
+    }
+    
+    private func setUpLayout() {
+        // Configure the cell layout
         layer.cornerRadius = LAYOUT_CONSTANTS.CORNER_RADIUS
         backgroundImageView.image = UIImage(named: IMAGE_CONSTANTS.HUMIDITY)
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.clipsToBounds = true
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(backgroundImageView)
+        
+        // Pin the image view to the edges of the cell
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        // Configure the humidity label
+        humidityLabel.translatesAutoresizingMaskIntoConstraints = false
+        humidityLabel.textAlignment = .center
+        humidityLabel.font = UIFont(name: AppConstants.FONT_STYLE, size: AppConstants.FONT_SIZE)
+        humidityLabel.textColor = .white
+        humidityLabel.numberOfLines = LAYOUT_CONSTANTS.NUMBER_OF_LINES
+        
+        // Add humidity label to the view
+        contentView.addSubview(humidityLabel)
+        
+        //Pin the humidity label to the center of the cell
+        NSLayoutConstraint.activate([
+            humidityLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            humidityLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+
     }
     
     func configure(_ weatherData : WeatherCardData) {
--- Project-Demo/TemperatureCollectionViewCell.swift
+++ Project-Demo/TemperatureCollectionViewCell.swift
@@ -3,10 +3,12 @@ import SDWebImage
 
 class TemperatureCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var backgroundImageView: UIImageView!
-    @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var weatherDescriptionLabel: UILabel!
-    @IBOutlet weak var weatherIcon: UIImageView!
+    let backgroundImageView = UIImageView()
+    let icon_temperaure_StackView = UIStackView()
+    let weatherInfoStackView = UIStackView()
+    let weatherIcon = UIImageView()
+    let temperatureLabel = UILabel()
+    let weatherDescriptionLabel = UILabel()
     
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -14,11 +16,72 @@ class TemperatureCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUpLayout()
+    }
+    
+    private func setUpLayout() {
+        // Configure the cell layout
         layer.cornerRadius = LAYOUT_CONSTANTS.CORNER_RADIUS
         backgroundImageView.image = UIImage(named: IMAGE_CONSTANTS.WEATHER_INFO)
         backgroundImageView.contentMode = .scaleAspectFill
         backgroundImageView.clipsToBounds = true
-    
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(backgroundImageView)
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        // Configure the weather icon
+        weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        weatherIcon.contentMode = .scaleAspectFit
+        weatherIcon.clipsToBounds = true
+        
+        // Configure the temperature label
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.textAlignment = .center
+        temperatureLabel.font = UIFont(name: AppConstants.FONT_STYLE, size: LAYOUT_CONSTANTS.TEMPERATURE_LABEL_FONT_SIZE)
+        temperatureLabel.textColor = .white
+        
+        // Configure the horizontal stack view (icon_temperaure_StackView)
+        icon_temperaure_StackView.axis = .horizontal
+        icon_temperaure_StackView.alignment = .center
+        
+        // add weather icon and temperature label to horizontal stack view (icon_temperaure_StackView)
+        icon_temperaure_StackView.addArrangedSubview(weatherIcon)
+        icon_temperaure_StackView.addArrangedSubview(temperatureLabel)
+        
+        // Configure the weather description label
+        weatherDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        weatherDescriptionLabel.textAlignment = .center
+        weatherDescriptionLabel.font = UIFont(name: AppConstants.FONT_STYLE, size: LAYOUT_CONSTANTS.WEATHER_DESCRIPTION_FONT_SIZE)
+        weatherDescriptionLabel.textColor = .white
+        
+        // Configure the vertical stack view (weatherInfoStackView)
+        weatherInfoStackView.axis = .vertical
+        weatherInfoStackView.alignment = .center
+        weatherInfoStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // add icon_temperaure_StackView and weather description label to vertical stack view (weatherInfoStackView)
+        weatherInfoStackView.addArrangedSubview(icon_temperaure_StackView)
+        weatherInfoStackView.addArrangedSubview(weatherDescriptionLabel)
+
+        contentView.addSubview(weatherInfoStackView)
+        
+        // Pin the weather info stack view to the center of the cell
+        NSLayoutConstraint.activate([
+            weatherInfoStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            weatherInfoStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        
+        // Set a size for the weather icon
+        NSLayoutConstraint.activate([
+            weatherIcon.widthAnchor.constraint(equalToConstant: LAYOUT_CONSTANTS.WEATHER_ICON_WIDTH),
+            weatherIcon.heightAnchor.constraint(equalToConstant: LAYOUT_CONSTANTS.WEATHER_ICON_HEIGHT)
+        ])
     }
     
     func configure(_ weatherData: WeatherCardData) {
